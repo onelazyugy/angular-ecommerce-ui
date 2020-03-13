4996c5a4c0c18462db99f2bec14ca8d08536c795
Viet <viet@Viets-MBP.attlocal.net>
2020-03-12 23:38:55 -0400

    start working on rendering items for electronics category

--- src/app/store/electronic/electronic.component.ts
+++ src/app/store/electronic/electronic.component.ts
@@ -6,10 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./electronic.component.css']
 })
 export class ElectronicComponent implements OnInit {
-
+  electronics = [
+    {'id': 1, 'name': 'iphone 11', 'link': 'www.google.com', 'imgurl': 'https://www.boostmobile.com/amp/img/iphone-11/IPH11BT64BLK/image.jpg'},
+    {'id': 2, 'name': 'iphone xs max', 'link': 'www.google.com', 'imgurl': 'https://fdn2.gsmarena.com/vv/pics/apple/apple-iphone-x-new-1.jpg'},
+    {'id': 3, 'name': 'iphone 11', 'link': 'www.google.com', 'imgurl': 'https://static.bhphoto.com/images/images500x500/apple_mvvk2ll_a_16_macbook_pro_late_1573663014_1520435.jpg'}
+  ]
   constructor() { }
 
   ngOnInit() {
+
   }
 
 }
--- src/app/store/store.module.ts
+++ src/app/store/store.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { StoreComponent } from './store.component';
 import { ElectronicComponent } from './electronic/electronic.component';
 import { AppRoutingModule } from '../app-routing.module';
@@ -13,6 +14,7 @@ import { ClothingComponent } from './clothing/clothing.component';
         ClothingComponent
     ],
     imports: [
+        CommonModule, //ngFor
         AppRoutingModule
     ]
 })
