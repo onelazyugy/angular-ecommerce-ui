ec080c18b2d6d26b52c803ede51efcdadfcc693e
Viet <viet@Viets-MacBook-Pro.local>
2020-03-13 23:58:00 -0400

    loop categories and toolbar menus

--- src/app/store/store.component.ts
+++ src/app/store/store.component.ts
@@ -6,7 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./store.component.css']
 })
 export class StoreComponent implements OnInit {
-
+  categories = [
+    {id: '1', name: 'ELECTRONICS', route: '/store/electronics', icon: 'shopping cart icon'},
+    {id: '2', name: 'BOOKS', route: '/store/books', icon: 'shopping cart icon'},
+    {id: '3', name: 'CLOTHING', route: '/store/clothes', icon: 'shopping cart icon'},
+  ];
   constructor() { }
 
   ngOnInit() {
--- src/app/toolbar/toolbar.component.ts
+++ src/app/toolbar/toolbar.component.ts
@@ -6,7 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./toolbar.component.css']
 })
 export class ToolbarComponent implements OnInit {
-  title = 'angular-ecommerce';
+  
+  toolbarItems = {
+    title: 'angular-ecommerce',
+    menus: [
+      {id: '1', name: 'HOME', route: '', icon: 'home icon'},
+      {id: '2', name: 'STORE', route: '/store', icon: 'shopping basket icon'},
+      {id: '3', name: 'CART', route: '/cart', icon: 'shopping cart icon'}
+    ]
+  }
   constructor() { }
 
   ngOnInit() {
