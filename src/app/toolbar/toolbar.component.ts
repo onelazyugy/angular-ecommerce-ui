46a686e7f4f5d1ff6ea0bde2240c94d419e58677
Viet <viet@Viets-MacBook-Pro.local>
2020-03-23 21:31:06 -0400

    modify home page to be stackable

--- src/app/toolbar/toolbar.component.ts
+++ src/app/toolbar/toolbar.component.ts
@@ -12,9 +12,7 @@ export class ToolbarComponent implements OnInit {
     menus: [
       {id: '1', name: 'HOME', route: '', icon: 'home icon'},
       {id: '2', name: 'STORE', route: '/store', icon: 'shopping basket icon'},
-      {id: '3', name: 'CART', route: '/cart', icon: 'shopping cart icon'},
-      {id: '4', name: 'ADMIN', route: '/admin', icon: 'user icon'},
-      {id: '5', name: 'SIGN OUT', route: '/sign-out', icon: 'sign-out icon'}
+      {id: '3', name: 'CART', route: '/cart', icon: 'shopping cart icon'}
     ]
   }
   constructor() { }
