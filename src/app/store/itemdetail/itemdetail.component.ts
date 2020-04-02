eaf5b15757a6c4f36db9e8a3b74948853aa192a8
Viet <viet@Viets-MBP.attlocal.net>
2020-04-01 23:02:48 -0400

    minor cosmetic

--- src/app/store/itemdetail/itemdetail.component.ts
+++ src/app/store/itemdetail/itemdetail.component.ts
@@ -16,6 +16,7 @@ export class ItemdetailComponent implements OnInit, OnDestroy {
   itemDetail: ItemDetail;
   itemDetailsRouteParam: any;
   qtySelected: number;
+  isAddItemToCartSuccess: boolean;
   qty = [{name: 1, value: 1}, {name: 2, value: 2}, {name: 3, value: 3}, {name: 4, value: 4}, 
         {name: 5, value: 5}, {name: 6, value: 6}, {name: 7, value: 7}, {name: 8, value: 8}, 
         {name: 9, value: 9}, {name: 10, value: 10}];
@@ -41,6 +42,12 @@ export class ItemdetailComponent implements OnInit, OnDestroy {
     const itemAdded = {item: this.itemDetail.item, qty: this.qtySelected};
     const cartDeatil = new CartDetail().deserialize(itemAdded);
     this.cartService.emitCart(cartDeatil);
+    //TODO: local storage?
+    this.isAddItemToCartSuccess = true;
+  }
+
+  close() {
+    this.isAddItemToCartSuccess = false;
   }
 
   selectChangeHandler(event) {
