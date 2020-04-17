a6dee4b4af9b61748a113b288cd92c71ac325202
Viet <viet@Viets-MBP.attlocal.net>
2020-04-17 16:25:39 -0400

    implemented idle timeout using ng-idle

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { NgIdleKeepaliveModule } from '@ng-idle/keepalive'; // this includes the core NgIdleModule but includes keepalive providers for easy wireup
+import { MomentModule } from 'angular2-moment'; // optional, provides moment-style pipes for date formatting
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -30,7 +32,9 @@ import { SignUpModule } from './signup/signup.module';
     CheckoutModule,
     CompleteModule,
     LoginModule,
-    SignUpModule
+    SignUpModule,
+    NgIdleKeepaliveModule.forRoot(),
+    MomentModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -3,6 +3,7 @@ import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
 import { Subscription } from 'rxjs';
 import { UserService } from '../service/user.service';
+import { IdleService } from '../service/idle.service';
 
 @Component({
   selector: 'app-login',
@@ -19,7 +20,7 @@ export class LoginComponent implements OnInit, OnDestroy {
   message: string;
   isError: boolean = false;
 
-  constructor(private router: Router, private userService: UserService) { }
+  constructor(private router: Router, private userService: UserService, private idleService: IdleService) { }
 
   ngOnInit() {
   }
@@ -35,6 +36,12 @@ export class LoginComponent implements OnInit, OnDestroy {
         // npm install --save @auth0/angular-jwt
         this.userService.emitLoginUserDetail(response.user);
         //TODO: if localStorage has lastRoute, then redirect user there, else navigate to /
+
+
+
+        //timeout idle
+        this.idleService.setUserLoggedIn(true)
+        //end idle
         this.router.navigate(['/']);
       } else {
         this.isError = true;
